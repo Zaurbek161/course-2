10e2ac8a46770019d0c8aa73489d60d6db6832a6
Zaurbek <zaurcheg07@gmail.com>
2024-03-21 17:42:33 +0300

    сделал домашку 2,7

--- game.js
+++ game.js
@@ -1,18 +1,46 @@
-function monthNumbers () {
-    let a = Number(prompt("Введите номер месяца"));
-    const number = Number (a)
-    if (number === 1 || number === 2 || number === 12) {
-        alert ("Зима");
-    } else if (number === 3 || number === 4 || number === 5) {
-        alert ("Весна");
-    } else if (number === 6 || number === 7 || number === 8) {
-        alert ("Лето");
-    } else if (number === 9 || number === 10 || number === 11) {
-            alert ("Осень");
-    } else {
-        alert ("Введен не верный номер месяца");
+//------времена года-------------
 
+// function monthNumbers () {
+//     let a = Number(prompt("Введите номер месяца"));
+//     const number = Number (a)
+//     if (number === 1 || number === 2 || number === 12) {
+//         alert ("Зима");
+//     } else if (number === 3 || number === 4 || number === 5) {
+//         alert ("Весна");
+//     } else if (number === 6 || number === 7 || number === 8) {
+//         alert ("Лето");
+//     } else if (number === 9 || number === 10 || number === 11) {
+//             alert ("Осень");
+//     } else {
+//         alert ("Введен не верный номер месяца");
+
+//     }
+// }
+
+// console.log(monthNumbers());
+
+
+
+//---------Запомни слова------
+function memorization() {
+    const wordsToRemember = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+
+    alert('Прочитай и запомни данные слова:\n\nЯблоко, Груша, Дыня, Виноград, Персик, Апельсин, Мандарин\n\nЕсли запомнил нажимай OK');
+
+    let checkingWords = prompt('А теперь напряги память и напиши, что запомнил(а) через пробел');
+
+    if (checkingWords === null || checkingWords.trim() === '') {
+        alert('Ты не ввел(а) ни одного слова. Попробуй еще раз!');
+        return;
+    }
+
+    const userWords = new Set(checkingWords.toLowerCase().split(' '));
+
+    const correctOrder = [...wordsToRemember].every(word => userWords.has(word.toLowerCase()));
+
+    if (correctOrder) {
+        alert('Молодец! Правильно!');
+    } else {
+        alert('Попробуй ещё раз');
     }
 }
-
-console.log (monthNumbers());
--- hw-2-7.js
+++ hw-2-7.js
@@ -1,2 +1,107 @@
+// //----------t-1-------------
 let text = 'js';
 console.log(text.toUpperCase());
+
+
+//----------t-2-------------  
+function filterStringsByPrefix(stringArray, prefix) {
+    prefix = prefix.toLowerCase();
+    let filteredArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+        let lowerString = stringArray[i].toLowerCase();
+
+        if (lowerString.startsWith(prefix)) {
+            filteredArray.push(stringArray[i]);
+        }
+    }
+    return filteredArray;
+}
+
+let arr = ["мама", "маша", "машина", "Петр", "трактор"];
+let prefix = "ма";
+let result = filterStringsByPrefix(arr, prefix);
+console.log(result);
+
+//-----------------t-3-----------------
+let number = 32.58884;
+let roundedDown = Math.floor(number);
+let rounderUp = Math.ceil(number);
+let rounderNearest = Math.round(number);
+console.log('До меньшего целого:', roundedDown);
+console.log('До большего целого', rounderUp);
+console.log('До ближайщего целого', rounderNearest);
+
+//-------------t-4----------------
+let numMaxMin = [52, 53, 49, 77, 21, 32];
+let max = Math.max(...numMaxMin);
+let min = Math.min(...numMaxMin);
+console.log('Максимальное значение:', max);
+console.log('Минимальное значение:', min);
+
+//-------------t-5-------------
+console.log(Math.round(Math.random() * 10 + 1));
+
+//-------------t-6-------------
+function generateRandomArray(num) {
+    if (typeof num !== 'number' || !Number.isInteger(num) || num <= 0) {
+        return "Введите положительное целое число";
+    }
+    const arrayLength = num / 2;
+    const randomArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+        randomArray.push(Math.floor(Math.random() * num));
+    }
+    return randomArray;
+}
+
+const inputNumber = 20;
+const resultArray = generateRandomArray(inputNumber);
+console.log(resultArray);
+
+//--------------t-7---------------
+function getRandomNumberInRange(x, y){
+    if (!Number.isInteger(x)|| !Number.isInteger(y)){
+        return "Введите целые числа";
+    }
+    if (x > y){
+        [x, y] = [y, x];
+    }
+    const randomNumber = Math.floor(Math.random() * (y - x + 1)) + x;
+    return randomNumber;
+}
+const upshot = getRandomNumberInRange(12, 45);
+console.log(upshot);
+
+//---------------t-8--------------
+let actualDate = new Date();
+console.log(actualDate);
+
+//----------t-9----------------
+let currentDate = new Date();
+currentDate.setDate(currentDate.getDate() + 73);
+let futureDate = currentDate.toLocaleDateString();
+
+console.log("Дата, которая наступит через 73 дня после текущей:", futureDate);
+
+//----------t-10--------------
+function formatDate(date) {
+    const months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    const weekday = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    const dayOfWeek = weekday[date.getDay()];
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+
+    const dateString = `Дата: ${day} ${month} ${year} — это ${dayOfWeek}.`;
+    const timeString = `Время: ${hours}:${minutes}:${seconds}`;
+
+    return dateString + '\n' + timeString;
+}
+
+const presentDay = new Date();
+const formattedDate = formatDate(presentDay);
+console.log(formattedDate);
