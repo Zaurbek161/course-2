3b4996ec1370fef81d25a8bf27eb63998b6c9ebb
Zaurbek <zaurcheg07@gmail.com>
2024-05-06 11:09:07 +0300

    2.5 исправления во временах года

--- game.js
+++ game.js
@@ -1,18 +1,18 @@
-function monthNumbers () {
-    let a = prompt("Введите номер месяца");
-    const number = Number (a)
-    if (number === 1 || number === 2 || number === 12) {
-        return "Зима";
-    } else if (number === 3 || number === 4 || number === 5) {
-        return "Весна";
-    } else if (number === 6 || number === 7 || number === 8) {
-        return "Лето";
-    } else if (number === 9 || number === 10 || number === 11) {
-            return "Осень";
-    } else {
-        return "Введен не верный номер месяца";
-
-    }
+function monthNumbers() {
+  let a = prompt("Введите номер месяца");
+  const number = Number(a);
+  if (number === 1 || number === 2 || number === 12) {
+    return "Зима";
+  } else if (number === 3 || number === 4 || number === 5) {
+    return "Весна";
+  } else if (number === 6 || number === 7 || number === 8) {
+    return "Лето";
+  } else if (number === 9 || number === 10 || number === 11) {
+    return "Осень";
+  } else {
+    return "Введен не верный номер месяца";
+  }
 }
 
-console.log (monthNumbers());
+console.log(monthNumbers());
+
